9d520d477f90b2f4190338676aa9a725fb2fc447
Smith <smithn10@seattleu.edu>
2018-03-29 13:42:52 -0700

    deleted sensitive info

--- LicenseFilter/LicenseFilter.py
+++ LicenseFilter/LicenseFilter.py
@@ -4,37 +4,54 @@ import cProfile
 
 
 # compares two tables to filter out lab machines from single user machines
-def compareTables(table_one, table_two):
-    with open(table_one, 'r', newline='') as t1, open(table_two, 'r', newline='') as t2:
-        t1Reader = csv.reader(t1, delimiter=',')
-        t2Reader = csv.reader(t2, delimiter=',')
+def compareSingleLicenses(currentUsers, dataUsers, multiUsers):
+    singleCompared = []
+    currently_licensed = currentUsers
+    stored_licensed = dataUsers
+    serializedUsers = multiUsers
+    for element in currently_licensed:
+        email = element[1]
+        for entry in stored_licensed:
+            username = entry[1]
+            if (checkEmail(email, username) == False):
+                singleCompared.append(entry)
+            else:
+                break
 
-        t1List = list(t1Reader)
-        t2List = list(t2Reader)
+    mergedSingleWriter(singleCompared, serializedUsers)
 
-        if (len(t1List) > len(t2List)):
-            compared_list = [license for license in t1List if license not in t2List]
-        else:
-            compared_list = [license for license in t2List if license not in t1List]
 
-    getLicences = writeCSV(compared_list)
+def tableProcessing(inFile):
+    with open(inFile, 'r', newline='') as full_table:
+        fullReader = csv.reader(full_table, delimiter=',')
 
-    single_list = getLicences[0]
-    multi_list = getLicences[1]
+        full_list = list(fullReader)
 
-    LicenseWriter(single_list)
-    LicenseWriter(multi_list)
+        writeToList = csvToList(full_list)
 
-def macTableProcessing(inFile):
-    inFile
+        single_list = writeToList[0]
+        serializable = writeToList[1]
+
+        return (single_list, serializable)
+
 
 def checkAdmin(username):
     m = re.match('^[A-a]dmin.*$|^[S-s]ystem.*$', username)
     return m
 
-#checks current entry to see if it is a single license user
+
+def checkEmail(email, username):
+    m = re.search('(^[^@]+)', email)
+    if m:
+        found = m.group(1)
+        return found == username
+    else:
+        return False
+
+
+# checks current entry to see if it is a single license user
 def filterSingle(row, currentList):
-    username = row[1]
+    username = row[1].lower()
     if (checkAdmin(username)):
         return False
     computername = row[0]
@@ -45,9 +62,10 @@ def filterSingle(row, currentList):
     else:
         return True
 
-#checks current entry to see if it is a serializable license
+
+# checks current entry to see if it is a serializable license
 def filterMulti(row, currentList):
-    username = row[1]
+    username = row[1].lower()
     computername = row[0]
     lastLicenseEntry = currentList[-1]
 
@@ -59,37 +77,38 @@ def filterMulti(row, currentList):
 
 # checks for single use and serialized machines
 # must contain column headers to function properly
-def writeCSV(inFile):
+def csvToList(inFile):
     single_list = []
     multi_list = []
 
     for row in inFile:
-        if len(single_list) == 0:
-            single_list.insert(0, row)
-            multi_list.insert(0, [row[0]])
-            continue
+        if (row[0] != ''):
+            if len(single_list) == 0:
+                single_list.insert(0, row)
+                multi_list.insert(0, [row[0]])
+                continue
 
-        isSingle = filterSingle(row, single_list)
+            isSingle = filterSingle(row, single_list)
 
-        if (isSingle):
-            if (filterMulti(row, single_list)):
+            if (isSingle):
+                if (filterMulti(row, single_list)):
+                    multi_list.append([single_list[-1][0]])
+                    single_list.remove(single_list[-1])
+                    multi_list.append([row[0]])
+                else:
+                    single_list.append(row)
+            else:
                 multi_list.append([single_list[-1][0]])
                 single_list.remove(single_list[-1])
                 multi_list.append([row[0]])
-            else:
-                single_list.append(row)
+            continue
         else:
-            multi_list.append([single_list[-1][0]])
-            single_list.remove(single_list[-1])
-            multi_list.append([row[0]])
-        continue
-
-    single_list.append("Single")
-    multi_list.append("Serialized")
+            continue
 
     return (removeDuplicates(single_list), removeDuplicates(multi_list))
 
-#removes duplicate entries from lists before writing to file
+
+# removes duplicate entries from lists before writing to file
 def removeDuplicates(duplicate):
     final_list = []
     for name in duplicate:
@@ -97,13 +116,22 @@ def removeDuplicates(duplicate):
             final_list.append(name)
     return final_list
 
-#writes user or machine entries into files
-def LicenseWriter(inputCSV):
-    with open(inputCSV[-1] + 'License.csv', 'w', newline='') as file:
+
+def mergedSingleWriter(singleCSV, multiCSV):
+    with open('MergedLicenses.csv', 'w', newline='') as file:
         singleWriter = csv.writer(file, delimiter=',', escapechar='')
 
-        for line in inputCSV:
+        singleWriter.writerow(["Single Use Licenses"])
+        for line in singleCSV:
             singleWriter.writerow(line)
+        singleWriter.writerow(["Number of Single User Licenses:" + str(len(singleCSV))])
+        singleWriter.writerow(["Serializable Licenses"])
+        for line in multiCSV:
+            singleWriter.writerow(line)
+        singleWriter.writerow(["Number of Serializable Licenses:" + str(len(multiCSV))])
 
 
-cProfile.run('compareTables("ComparedTableTest2.csv", "ComparedTableTest.csv")')
+licensed = tableProcessing("Current Users Licensed to Adobe CC.csv")
+notLicensed = tableProcessing("SCCMAdobeList.csv")
+
+compareSingleLicenses(licensed[0], notLicensed[0], notLicensed[1])
